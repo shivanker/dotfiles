ce581fd36cd0ac3c76e4ff41ec7231006e0f37d9
Shivanker Goel <shivanker.goel@gmail.com>
2023-04-24 23:39:40 +0000

    Bugfixes

--- unpack.sh
+++ unpack.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
+# 'Unpack' all git sub-repos
+find . -name '.git.orig' -exec bash -c 'mv $0 ${0/.git.orig/.git}' {} \;
+
 # No need for .git in homedir
 mv .git .git.orig
 
-# 'Unpack' all git sub-repos
-find . -name '.git.orig' -exec bash -c 'mv $0 ${0/.git.orig/.git}' {} \;
